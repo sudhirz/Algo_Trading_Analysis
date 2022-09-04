d213e4568e56c4484c69403d23d3bfed58800f53
sudhirz <71525163+sudhirz@users.noreply.github.com>
2022-09-03 20:49:40 -0500

    Update stock_screener_SMA.py

--- stock_screener_SMA.py
+++ stock_screener_SMA.py
@@ -48,23 +48,23 @@ EMA_screened_list = []
 def send_email(email_message, email_subject, attachment=None):
   msg = email.message_from_string(", ".join(email_message))
   msg = MIMEMultipart("alternative")
-  msg['From'] = 'mesudhir@gmail.com'
-  msg['To'] = 'mesudhir@gmail.com'
+  msg['From'] = 'xxxxxxx@gmail.com'
+  msg['To'] = 'xxxxxxx@gmail.com'
   msg['Subject'] = email_subject
 
   if attachment != None:
     part2 = MIMEText(attachment, "html")
     msg.attach(part2)
 
-  email_from = 'mesudhir@gmail.com'
-  email_to = 'mesudhir@gmail.com'
+  email_from = 'xxxxxxx@gmail.com'
+  email_to = 'xxxxxxx@gmail.com'
   s = smtplib.SMTP("smtp.gmail.com", 587)
   ## for yahoo mail user: s = smtplib.SMTP("smtp.mail.yahoo.com",587)
   ## for hotmail user: s = smtplib.SMTP("smtp.live.com",587)
   s.ehlo()
   s.starttls()
   s.ehlo()
-  s.login(email_from, "gqvfbxjxkggllmwv")
+  s.login(email_from, "fffgqvyyyfbxjxkggllmwv")
   s.sendmail(email_from, [email_to], msg.as_string())
   s.quit()
 
